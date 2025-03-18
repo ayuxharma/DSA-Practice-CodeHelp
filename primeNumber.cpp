40326bb2028d6c4a9d5c36b2fe8322b15781d7ca
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-03-18 22:22:27 +0530

    Add files via upload

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
 using namespace std;
+// 0 -> Not a Prime no.
+// 1 -> Prime no.
+
+bool isPrime(int n ) {
+   for(int i = 2; i < n; i++){
+       if(n % i == 0) {
+       //divide hogya h , not a prime no.
+          return 0;
+        }
+   }
+   return 1;
+}
 
 int main() {
+   
+   int n ;
+   cin >> n;
+   if(isPrime(n)) {
+        cout <<" is a Prime number " <<endl;
+   }
+   else{
+       cout<< "Not a prime number "<<endl;
 
-    int n;
-    cin >> n;
-
-    int i = 2;
-
-    while(i<n) {
-        //divide hogya, not prime
-        if(n%i==0) {
-            cout<< " Not prime for "<< i << endl;
-        }
-        else{
-            cout<<" Prime for "<< i <<endl;
-        }
-        i = i + 1;
-    }
-
+   }
 }
