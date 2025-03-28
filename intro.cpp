e55be88b7023cd4eeb315e794db262dc79ae5b3c
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-03-28 19:18:52 +0530

    Add files via upload

--- intro.cpp
+++ intro.cpp
@@ -1,105 +1,32 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
 
-char toLowerCase(char ch) {
-    if(ch >='a' && ch <='z')
-        return ch;
-    else{
-        char temp = ch - 'A' + 'a';
-        return temp;
-    }
-}
+int main () {
 
-bool checkPalindrome(char a[], int n) {
-    int s = 0;
-    int e = n-1;
+    int num  = 5 ;
 
-    while(s<=e) {
-        if(toLowerCase( a[s] ) != toLowerCase( a[e] ) )
-        {
-            return 0;       
-        }
-        else{
-            s++;
-            e--;
-        }
-    }
-    return 1;
-}
+    cout << num << endl;
 
-void reverse(char name[], int n) {
-    int s=0;
-    int e = n-1;
+    // address of Operator - &
 
-    while(s<e) {
-        swap(name[s++], name[e--]);
-    }
-}
+    cout <<" address of num is " << &num << endl;
 
-int getLength(char name[]) {
-    int count = 0;
-    for(int i = 0; name[i] != '\0'; i++) {
-        count++;
-    }
+    int *ptr = &num;
 
-    return count;
-}
+    cout << "Address is : " << ptr << endl;
+    cout << "value is : " << *ptr << endl;
 
-char getMaxOccCharacter(string s) {
+    double d = 4.3;
+    double *p2 = &d;
 
-    int arr[26] = {0};
+    cout << "Address is : " << p2 << endl;
+    cout << "value is : " << *p2 << endl;
+
+    cout << " size of integer is " << sizeof(num) << endl;
+    cout << " size of pointer is " << sizeof(ptr) << endl;
+    cout << " size of pointer is " << sizeof(p2) << endl;
 
 
-    //create an array of count of characters
-    for(int i=0; i<s.length(); i++) {
-        char ch = s[i];
-        //lowercase
-        int number = 0;
-        number = ch - 'a';
-        arr[number]++;
-    }
-
-    //find maximum occ character
-    int maxi = -1, ans = 0;
-    for(int i=0;i<26; i++) {
-        if(maxi < arr[i]) {
-            ans = i;
-            maxi = arr[i];
-        }
-    }
-    
-
-    return 'a'+ans;
-
-}
-
-int main( ) {
-/*
-    char name[20];
-
-    cout << "Enter your name " << endl;
-    cin >> name;
-    //name[2] = '\0';
-
-    cout << "Your name is ";
-    cout << name << endl;
-    int len = getLength(name);
-    cout << " Length: " << len << endl;
-
-    reverse(name, len);
-    cout << "Your name is ";
-    cout << name << endl;
-
-    cout <<" Palindrome or Not: " << checkPalindrome(name, len) << endl;
-
-    cout << " CHARACTER is " << toLowerCase('b') << endl;
-    cout << " CHARACTER is " << toLowerCase('C') << endl;
-    */
-
-   string s;
-   cin >> s;
-
-    cout << getMaxOccCharacter(s) << endl;
 
     return 0;
 }
